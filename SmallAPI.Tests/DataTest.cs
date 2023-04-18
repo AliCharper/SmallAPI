61a39fb6ac2a391505f1e21d47e96525b03209ac
AliKolahdoozan <alikolahdoozan@gmail.com>
2023-04-19 00:08:05 +0800

    Chaning the expected Test Value to 9

--- SmallAPI.Tests/DataTest.cs
+++ SmallAPI.Tests/DataTest.cs
@@ -9,7 +9,7 @@ namespace TinyApi.Tests
         [Fact]
         public void Number_Of_Countries_ShouldBe_8()
         {
-            Assert.Equal(8, Countries.CountryList.Count());
+            Assert.Equal(9, Countries.CountryList.Count());
         }
     }
 }
